02bef1aec0f643558349933d84ce0b19672c711f
sidqi <sidqiarasyid31@gmail.com>
2024-12-11 20:58:12 +0700

    making some function in header

--- Sosmed.h
+++ Sosmed.h
@@ -1,5 +1,41 @@
 #ifndef SOSMED_H_INCLUDED
 #define SOSMED_H_INCLUDED
+#include <iostream>
+#define idVertex(v) v->idVertex
+#define nextVertex(v) v->nextVertex
+#define firstEdge(v) v->firstEdge
+#define firstVertex(G) G.firstVertex
+
+
+using namespace std;
+
+typedef struct userNode *adrUser;
+typedef struct edge *adrEdge;
+
+struct userNode{
+    string userId;
+    string username;
+    int userAge;
+    adrUser nextUser;
+    adrEdge firstEdge;
+};
+
+struct edge {
+    string edgeId;
+    adrEdge nextEdge;
+};
+
+struct graph{
+    adrUser firstNode;
+};
+
+void createUser(string id, adrUser adress);
+void createGraph(graph G);
+void addUsertoGraph(graph G, string id);
+void showGraph(graph G);
+//deleteUser
+//addFriend
+//deleteFriend
 
 
 
